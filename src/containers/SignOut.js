7b0dce2dcca218513f5f054bd68c260771011168
priyanka931 <priyankachauhan931.com>
2020-05-04 14:05:01 +0530

    Cleaned up

--- src/containers/SignOut.js
+++
@@ -1,7 +0,0 @@
-import { connect } from 'react-redux'
-
-const mapDispacthFromProps = dispatch => ({
-    onSignOut: () => dispatch(signOut())
-})
-
-export default connect(null, mapDispacthFromProps)(SignOut)
